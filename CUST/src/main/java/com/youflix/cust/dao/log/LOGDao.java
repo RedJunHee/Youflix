de853bf26be8c72044c59c47fc18d731675758c2
DESKTOP-A7LO2BG\pc <redjoon10@gmail.com>
2021-02-16 09:53:32 +0900

    git 초기화

--- CUST/src/main/java/com/youflix/cust/dao/log/LOGDao.java
+++
@@ -1,9 +0,0 @@
-package com.youflix.cust.dao.log;
-
-import java.util.HashMap;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface LOGDao {
-	public void SendServiceLog(HashMap<String, String> params) throws Exception;
-}
--- CUST/src/main/java/com/youflix/cust/model/BaseModel.java
+++
@@ -1,7 +0,0 @@
-package com.youflix.cust.model;
-
-public interface BaseModel {
-
-	String toString();
-	
-}
