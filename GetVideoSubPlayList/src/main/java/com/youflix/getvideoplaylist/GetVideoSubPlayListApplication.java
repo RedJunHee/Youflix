38881245f2f0b1e38bc4d596bd3de5c37923a176
DESKTOP-A7LO2BG\pc <redjoon10@gmail.com>
2021-03-05 16:27:47 +0900

    ..

---
+++ GetVideoSubPlayList/src/main/java/com/youflix/getvideoplaylist/GetVideoSubPlayListApplication.java
@@ -0,0 +1,13 @@
+package com.youflix.getvideoplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetVideoSubPlayListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetVideoSubPlayListApplication.class, args);
+	}
+
+}
---
+++ GetVideoSubPlayList/src/test/java/com/youflix/getvideoplaylist/GetVideoSubPlayListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youflix.getvideoplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetVideoSubPlayListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
